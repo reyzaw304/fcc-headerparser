3d788063e5e9b44c9ac8a925bfbde281e9f7bf39
reyzaw304 <57722622+reyzaw304@users.noreply.github.com>
2025-09-10 20:33:46 +0900

    Implement /api/whoami endpoint

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.set('trust proxy', true)
+
+// request header parser API endpoint
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get("Accept-Language"), // wrapper for req.headers['accept-language']. req.header("Accept-Language") also works.
+    software: req.get("User-Agent")
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
